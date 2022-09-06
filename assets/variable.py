b4d8bd26340784077c46af1551cbacb49eb8177b
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2022-09-06 17:32:07 +0530

    Delete variable.py

--- assets/variable.py
+++
@@ -1,9 +0,0 @@
-# Stored all variables necessary for the project
-
-loginUrl = "https://console.uat.asians.group/#/domain/list"
-
-mail = "nitinkr@gmail.com"
-pswd = "pass#123"
-
-mail_new = "krnitin@gmail.com"
-pswd_new = "Pass@123"
